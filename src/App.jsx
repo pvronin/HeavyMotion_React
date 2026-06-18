f7ea1c8fe4451f4612044d7d7f65aac8ad3fa8e9
ALI <emeil.seyyed.arm@gmail.com>
2026-06-18 15:24:52 +0330

    bug fix for deploy adn build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { GsapBox } from './components/Gsapbox';
+import { GsapBox } from './components/GsapBox';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
